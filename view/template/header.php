7ab11803c0e77ff42960d3a047439fc2ae4615d7
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-10-30 15:12:13 +0100

    work on header css

--- view/template/header.php
+++ view/template/header.php
@@ -1,7 +1,9 @@
-<header>
-  <h1></h1>
-  <a href="home.php" class="btn btn-primary">Home</a>
-  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseNewVehicle" aria-expanded="false" aria-controls="collapseNewVehicle">
-    New Vehicle
-  </button>
+<header class="justify-content-between">
+  <h1 class="col-8">Car Handle</h1>
+  <div class="header_btn align-items-end col-4 justify-content-end">
+    <a href="home.php" class="btn btn-primary">Home</a>
+    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseNewVehicle" aria-expanded="false" aria-controls="collapseNewVehicle">
+      New Vehicle
+    </button>
+  </div>
 </header>
